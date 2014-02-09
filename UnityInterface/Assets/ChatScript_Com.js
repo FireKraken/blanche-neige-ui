0077f4f2f95768f01738e7de605ebe7a9a1a0f77
Ashwath George <ashwath.george@gmail.com>
2014-02-09 16:06:47 -0500

    Added "log" button
    With visibility toggle

--- UnityInterface/Assets/ChatScript_Com.js
+++ UnityInterface/Assets/ChatScript_Com.js
@@ -1,16 +1,16 @@
 ﻿#pragma strict
 
 private var ChatScripUrl= "http://127.0.0.1/chatscriptclient.php?";
-private var userInput : String = "Hello young woman";
-private var consoleText:String = "Last thing said";
-private var scrollPosition:Vector2;
+private var userInput : String = "[Type/dictate your answer]";
+private var consoleText : String = "Log";
+private var scrollPosition : Vector2 = Vector2.zero;
+private var showLog : boolean = false;
 
 function Start () {
 
 }
 
 function OnGUI(){
-
 
 	if (Event.current.type == EventType.KeyDown && (Event.current.keyCode == KeyCode.Return || Event.current.keyCode == KeyCode.Return)){
 		consoleText = consoleText+"\n"+userInput;
@@ -19,21 +19,38 @@ function OnGUI(){
 	
 	}
 	GUILayout.BeginArea(Rect (Screen.width *0.1, Screen.height *0.1, Screen.width *0.8, Screen.height *0.8));	
+	
 	GUILayout.BeginVertical();
-		scrollPosition = Vector2(0,1000000);
+	
+		scrollPosition.y = 1000000;
 		GUILayout.BeginScrollView (scrollPosition);
-		GUI.skin.box.wordWrap = true; // set the wordwrap on for box only.
-		GUILayout.Box(consoleText); // just your message as parameter.
-		GUILayout.EndScrollView ();
-		userInput = GUILayout.TextField ( userInput);
-		if(GUILayout.Button("Reset"))
+		GUI.skin.box.wordWrap = true; // set the wordwrap on for box only.			
+		if(showLog)
 		{
-			 postMessage(":reset");
-		}
-		if(GUILayout.Button("Rebuild")){
-			postMessage(":build 1");
-		}
+			GUILayout.Box(consoleText); // just your message as parameter.
+		}		
+		GUILayout.EndScrollView ();
+		
+		userInput = GUILayout.TextField ( userInput);
+		
+		GUILayout.BeginHorizontal();
+		
+			if(GUILayout.Button("Log"))
+			{
+				showLog = !showLog;
+			}
+			if(GUILayout.Button("Reset"))
+			{
+				 postMessage(":reset");
+			}
+			if(GUILayout.Button("Rebuild")){
+				postMessage(":build 1");
+			}
+			
+		GUILayout.EndHorizontal();
+		
 	GUILayout.EndVertical();
+	
 	GUILayout.EndArea();
 
 
