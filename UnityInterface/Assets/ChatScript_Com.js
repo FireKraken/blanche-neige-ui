72345a82053c218c35975ae9766e89588ff3fc8e
Ashwath George <ashwath.george@gmail.com>
2014-02-11 14:01:01 -0500

    Modifications to "log"
    Fixed scrollbar and modified consoleText strings to provide a proper
    distinction between user dialogue and bot dialogue in the log.

--- UnityInterface/Assets/ChatScript_Com.js
+++ UnityInterface/Assets/ChatScript_Com.js
@@ -2,7 +2,7 @@
 
 private var ChatScripUrl= "http://127.0.0.1/chatscriptclient.php?";
 private var userInput : String = "[Type/dictate your answer]";
-private var consoleText : String = "Log";
+private var consoleText : String;
 private var scrollPosition : Vector2 = Vector2.zero;
 private var showLog : boolean = false;
 
@@ -13,7 +13,7 @@ function Start () {
 function OnGUI(){
 
 	if (Event.current.type == EventType.KeyDown && (Event.current.keyCode == KeyCode.Return || Event.current.keyCode == KeyCode.Return)){
-		consoleText = consoleText+"\n"+userInput;
+		consoleText = consoleText+"\n[You] said: "+userInput;
         postMessage(userInput);
         userInput = "";	
 	
@@ -21,17 +21,24 @@ function OnGUI(){
 	GUILayout.BeginArea(Rect (Screen.width *0.1, Screen.height *0.1, Screen.width *0.8, Screen.height *0.8));	
 	
 	GUILayout.BeginVertical();
-	
-		scrollPosition.y = 1000000;
-		GUILayout.BeginScrollView (scrollPosition);
-		GUI.skin.box.wordWrap = true; // set the wordwrap on for box only.			
+			
+		GUI.skin.box.wordWrap = true; // set the wordwrap on for box only.
+		GUI.skin.box.alignment = TextAnchor.LowerLeft;
+		GUI.skin.label.alignment = TextAnchor.UpperCenter;
+		
 		if(showLog)
 		{
-			GUILayout.Box(consoleText); // just your message as parameter.
-		}		
-		GUILayout.EndScrollView ();
+			GUILayout.Label("Log", GUILayout.Width(250));
+			
+			scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(275));			
+			GUILayout.Box(consoleText, GUILayout.Width(250)); // just your message as parameter.			
+			GUILayout.EndScrollView ();
+			
+		}
 		
-		userInput = GUILayout.TextField ( userInput);
+		GUILayout.FlexibleSpace();
+		
+		userInput = GUILayout.TextField(userInput);
 		
 		GUILayout.BeginHorizontal();
 		
@@ -69,6 +76,6 @@ function postMessage(message:String){
     print(msgURL);
     var w = WWW(msgURL);
     yield w;	
-    consoleText = consoleText+"\n"+ w.text;
+    consoleText = consoleText+"\n[Snow White] said: "+w.text;
 
 }
