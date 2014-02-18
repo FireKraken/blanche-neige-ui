9f66e6d49139725c53e56f4b96596d663196081a
Ashwath George <ashwath.george@gmail.com>
2014-02-18 13:35:46 -0500

    Added User/Bot Text Bubbles
    Text bubble positions are determined by User_TextBoxTarget and
    Bot_TextBoxTarget GameObjects respectively.
    
    Also cleaned up some comments

--- UnityInterface/Assets/ChatScript_Com.js
+++ UnityInterface/Assets/ChatScript_Com.js
@@ -2,9 +2,18 @@
 
 private var ChatScripUrl= "http://127.0.0.1/chatscriptclient.php?";
 private var userInput : String = "[Type/dictate your answer]";
+private var userText : String = null;
+private var botOutput : String = null;
 private var consoleText : String;
 private var scrollPosition : Vector2 = Vector2.zero;
 private var showLog : boolean = false;
+
+public var userTarget : Transform; // attach User_TextBoxTarget here
+public var botTarget : Transform; // attach Bot_TextBoxTarget here
+private var userBubble = Rect(0,0,500,100);
+private var botBubble = Rect(0,0,500,100);
+private var offset =  Vector2(0, 1.5); // height above the target position
+
 // private var timeStamp : System.DateTime;
 // private var currentTime : String = null;
 
@@ -27,15 +36,16 @@ function OnGUI()
 		
 		consoleText = consoleText+"\n[You] said: "+userInput;
         postMessage(userInput);
+        userText = userInput;
         scrollPosition.y = Mathf.Infinity;
         userInput = "";
 	}
 	GUILayout.BeginArea(Rect (Screen.width *0.1, Screen.height *0.1, Screen.width *0.8, Screen.height *0.8));	
 	
 	GUILayout.BeginVertical();
-			
+				
 		GUI.skin.box.wordWrap = true; // Set the wordwrap on for box only.
-		GUI.skin.box.alignment = TextAnchor.LowerLeft; // Text alignment for boxes
+		GUI.skin.box.alignment = TextAnchor.MiddleLeft; // Text alignment for boxes
 		GUI.skin.label.alignment = TextAnchor.UpperCenter; // Text alignment
 		
 		if(showLog) // Manages log window elements
@@ -43,10 +53,22 @@ function OnGUI()
 			GUILayout.Label("Log", GUILayout.Width(250));
 			
 			scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(275));			
-			GUILayout.Box(consoleText, GUILayout.Width(250)); // Just your message as parameter.			
+			GUILayout.Box(consoleText, GUILayout.Width(250)); // Post console text to the log.			
 			GUILayout.EndScrollView ();
 						
 		}
+		
+		// 	User text bubble		
+		var userPoint = Camera.main.WorldToScreenPoint(userTarget.position);
+	    userBubble.x = userPoint.x;
+	    userBubble.y = userPoint.y;
+	    GUI.Box(userBubble, userText);
+	    
+	    // Bot text bubble
+	    var botPoint = Camera.main.WorldToScreenPoint(botTarget.position);
+	    botBubble.x = botPoint.x;
+	    botBubble.y = botPoint.y; // bottom left corner set to the 3D point
+	    GUI.Box(botBubble, botOutput); // display its name, or other string
 		
 		GUILayout.FlexibleSpace();
 		
@@ -73,9 +95,6 @@ function OnGUI()
 	GUILayout.EndVertical();
 	
 	GUILayout.EndArea();
-
-
-
 }
 
 function Update () 
@@ -90,7 +109,7 @@ function postMessage(message:String)
     print(msgURL);
     var w = WWW(msgURL);
     yield w;
-    
+    botOutput = w.text; // Retrieve bot response for display in text bubble
      /* // Recording time stamps for bot replies
     timeStamp = System.DateTime.Now;
 	currentTime = String.Format("{0:D2}:{1:D2}:{2:D2}", timeStamp.Hour, timeStamp.Minute, timeStamp.Second);
